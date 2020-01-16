d88cab3aa15a7d73a7dae3e6b75d630f4c7819a2
sunny22071995 <sunny22071995@gmail.com>
2020-01-16 12:30:09 +0530

    island program

---
+++ matrix.js
@@ -0,0 +1,45 @@
+let numIslands = function(grid) {
+
+    let markIsland = function(grid, x, y, visited) {
+      if(x < 0 || x > grid.length - 1 || y < 0 || y > grid[x].length - 1) {
+        return
+      }
+      if(visited[x][y] === true) {
+        return
+      }
+      visited[x][y] = true;
+      if(grid[x][y] === '0') {
+        return
+      }
+      markIsland(grid, x - 1, y, visited);
+      markIsland(grid, x + 1, y, visited);
+      markIsland(grid, x, y - 1, visited);
+      markIsland(grid, x, y + 1, visited);
+    };
+    
+      let visited = []
+      for(let i = 0; i < grid.length; i++) {
+        visited[i] = []
+      }
+      let count = 0
+      for(let x = 0; x < grid.length; x++) {
+        for(let y = 0; y < grid[x].length; y++) {
+          if(!visited[x][y] && grid[x][y] === '1') {
+            count++
+            markIsland(grid, x, y, visited)
+          }
+          visited[x][y] = true
+        }
+      }
+      return count
+    }
+    
+    let example_matrix = [
+      ['1','1','0','0','1'],
+      ['1','1','0','0','0'],
+      ['0','0','0','0','0'],
+      ['1','0','0','1','1'],
+      ['1','0','0','1','1']
+    ]
+    console.log(numIslands(example_matrix))
+
